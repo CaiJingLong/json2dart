e74dcfd54d1e5eb4af70807db5b9904d5fff1f93
Caijinglong <cjl_spy@163.com>
2019-08-20 11:03:49 +0800

    Update file to docs

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
+dist=docs
 webdev build
-rm -rf dist
-mkdir dist
-cp build/*.ico dist
-cp build/*.jpg dist
-cp build/*.html dist
-cp build/*.js dist
-cp build/*.css dist
+rm -rf $dist
+mkdir $dist
+cp build/*.ico $dist
+cp build/*.jpg $dist
+cp build/*.html $dist
+cp build/*.js $dist
+cp build/*.css $dist
+
+echo "copy path to $dist"
